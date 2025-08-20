88b0e357a89dd4416ec9f43c4d4f37c48e99e09b
Wan Muzaffar <wanmuz86@gmail.com>
2025-08-20 09:12:33 +0700

    first commit

---
+++ src/components/ComponentA.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const ComponentA: React.FC = () => {
+ return <div>Component A Loaded</div>;
+};
+
+
+export default ComponentA;
---
+++ src/components/ComponentB.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const ComponentB: React.FC = () => {
+ return <div>Component B Loaded</div>;
+};
+
+
+export default ComponentB;
